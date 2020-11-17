bbb24cffaea88ff9c8a698822256c61b7ac130c6
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-11-17 19:03:38 +0530

    imported semantic.min.css

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// semantic-ui min.css
+import 'semantic-ui-css/semantic.min.css';
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
