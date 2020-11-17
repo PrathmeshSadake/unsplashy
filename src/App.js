553a9bb1b714cf5c6563c2485c48e8d2cd1384d1
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-11-17 19:03:20 +0530

    imported Header component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Header from './components/Header/Header';
+
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <Header />
+            </div>
+        );
+    };
+};
+
+export default App;
