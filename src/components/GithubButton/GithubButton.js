e17961189d08557bfbef1d2c5493be42e9e6f883
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-11-17 20:30:10 +0530

    Added GithubButton

---
+++ src/components/GithubButton/GithubButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Icon } from 'semantic-ui-react'
+
+import './github-button.css';
+
+const GithubButton = () => (
+  <div className="github-container">
+      <a href="https://www.github.com/PrathmeshSadake/unsplashy" target="_blank" rel="noreferrer noopener">
+         <Icon  className="github" name='github' size='huge'/>
+      </a>
+  </div>
+)
+
+export default GithubButton;
