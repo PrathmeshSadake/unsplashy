5559e25793ca8064c9c1a72a1723a973fcafc90f
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-11-19 00:17:23 +0530

    Formatted code

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
 import React, { Component } from 'react';
+
 import Header from './components/Header/Header';
 import SearchBar from './components/SearchBar/SearchBar';
 
-import Unsplash from './api/unsplash';
+import './App.css';
 
 class App extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            images : []
-        }
-    }
-
-    onSearchSubmit = async (input) => {
-       const response = await Unsplash.get('search/photos', {
-            params: { 
-                query: input,
-                per_page: 50,
-                // orientation: 'landscape',
-             },
-        });
-
-        this.setState({ images: response.data.results });
-    }
-
     render(){
         return(
             <div>
                 <Header />
-                <SearchBar onSubmit={this.onSearchSubmit}/>
-                {
-                    this.state.images.map((image)=>{
-                        return <img key={image.id} src={image.urls.regular} alt={image.description}/>
-                    })
-                }
+                <SearchBar/>
             </div>
         );
     };
