a0cf20c491235ace15e64e9de7984f1a78001452
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-11-18 18:56:40 +0530

    Moved code to InputForm component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,37 +1,15 @@
 import React, { Component } from 'react';
-import { Input } from 'semantic-ui-react';
-
+import InputForm from '../InputForm/InputForm';
 import './search-bar.css';
 
 class SearchBar extends Component{
-  constructor(props){
-    super(props);
-    this.state ={
-        input : '',
-    };
-}
-
-onInputChange = (event) => {
-    this.setState({ input : event.target.value });
-}
-
-handleFormSubmit = (event) =>{
-    //prevent default submitting of form
-    event.preventDefault();
-    //callback from app.js component
-    this.props.onSubmit(this.state.input);
-}
-
-
-
+  
     render(){
         return(
             <div className="ui container">
               <div className="inner-container">
                 <h1 className="heading">The best source for freely-useable photos shared by talented creators.</h1>
-                <form onSubmit={this.handleFormSubmit}>
-                    <Input value={this.state.input} onChange={this.onInputChange} action='Search' className="search-bar" fluid placeholder='Search for free images' icon='search' iconPosition='left' />
-                </form>
+                <InputForm />
                 <h3 className="license">Read more about the  <a href="https://unsplash.com/license">Unsplash License</a></h3>
               </div>
             </div>
