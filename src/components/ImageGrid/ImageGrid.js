5e61988bbaba3f44d0987e922f210fa0fbd2b367
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-11-19 00:18:58 +0530

    Added infinite scroll functionality

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+import './image-grid.css';
 
 class ImageGrid extends React.Component{
     render(){
         return(
-            <div>
-                <div className="image-grid">
+         <div className="grid-container">
+          <InfiniteScroll style={{overflow: 'hidden'}} dataLength={this.props.images.length} next={this.props.next} hasMore={true} loader={<h1>loading...</h1>}>
+          <div className="image-grid">
           {this.props.images.map((image, index) => (
-            <a className="image" key={index} href="image.links.html" target="_blank" rel="noopener noreferrer" >
+            <div className="image">
+              <a key={index} href={image.links.html} target="_blank" rel="noopener noreferrer" >
               <img src={image.urls.regular} alt={image.alt_description} />
             </a>
-          ))}
-        </div>
             </div>
+          ))}
+          
+          </div>
+          </InfiniteScroll>
+         </div>
         )
     }
 }
