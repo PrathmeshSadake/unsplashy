57aabd086a30445d185f519475b61efc82e503e5
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-11-19 00:18:33 +0530

    Removed testing comments and formatted code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
-
 import { Image } from 'semantic-ui-react'
 
-// import Unsplash from '../../api/unsplash';
-import './header.css';
 import GithubButton from '../GithubButton/GithubButton';
-// import SearchBar from '../SearchBar/SearchBar';
-// import ImageGrid from '../ImageGrid/ImageGrid';
 
+import './header.css';
 
 class Header extends Component{
-
-
-
     render(){
         return(
             <div className="header" >
                 <Image className="img" fluid/>
-            
-                {/* {console.log(this.state.images.length)} */}
                 <GithubButton/>
-                {/* {console.log(this.state.images)}
-                <img src={} alt={}/> */}
             </div>
         )
     }
