0ab7147e2a43397c6e670864eb3baf5ee28a02ee
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-11-19 00:19:30 +0530

    minor fixes

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+
 import InputForm from '../InputForm/InputForm';
+
 import './search-bar.css';
 
 class SearchBar extends Component{
   
     render(){
         return(
+          <div>
             <div className="ui container">
               <div className="inner-container">
                 <h1 className="heading">The best source for freely-useable photos shared by talented creators.</h1>
+                </div>
+                </div>
                 <InputForm />
-                <h3 className="license">Read more about the  <a href="https://unsplash.com/license">Unsplash License</a></h3>
-              </div>
+              
             </div>
         )
     }
