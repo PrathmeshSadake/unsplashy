9cf12f61b730bf1deeb8efdd4d34cc6d249961cc
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-11-19 00:14:25 +0530

    Deleted axios instance file

--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import accessKey from './unsplashAccessKey';
-
-const Unsplash = axios.create({
-    baseURL: 'https:/api.unsplash.com',
-    headers: { Authorization: `Client-ID ${accessKey}` }
-});
-
-export default Unsplash;
