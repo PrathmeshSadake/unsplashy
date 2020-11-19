2ba83523503376328574a035b136dc3b8c32089f
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-11-19 19:45:12 +0530

    removed semantic ui styling

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,7 +9,7 @@ class SearchBar extends Component{
     render(){
         return(
           <div>
-            <div className="ui container">
+            <div className="container">
               <div className="inner-container">
                 <h1 className="heading">The best source for freely-useable photos shared by talented creators.</h1>
                 </div>
