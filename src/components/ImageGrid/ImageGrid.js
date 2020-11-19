ea069f712f3b38734b5ab6ed87bc6a9b7432c8e8
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-11-19 16:26:32 +0530

    Added loader from loading.io

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -2,12 +2,15 @@ import React from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 import './image-grid.css';
+import './loader.css';
 
 class ImageGrid extends React.Component{
     render(){
         return(
          <div className="grid-container">
-          <InfiniteScroll style={{overflow: 'hidden'}} dataLength={this.props.images.length} next={this.props.next} hasMore={true} loader={<h1>loading...</h1>}>
+          <InfiniteScroll style={{overflow: 'hidden'}} dataLength={this.props.images.length} next={this.props.next} hasMore={true} loader={<div className="loader"><div class="loadingio-spinner-dual-ball-7vhy8l1xg7t"><div class="ldio-9ro5rlpgzop">
+<div></div><div></div><div></div>
+</div></div></div>}>
           <div className="image-grid">
           {this.props.images.map((image, index) => (
             <div className="image">
