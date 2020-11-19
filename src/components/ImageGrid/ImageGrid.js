7a47b36d6094163e91dfae94d5cdc092408e11df
Prathmesh Sadake <prathmeshsadake@gmail.com>
2020-11-19 19:45:25 +0530

    minor changes

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -8,18 +8,17 @@ class ImageGrid extends React.Component{
     render(){
         return(
          <div className="grid-container">
-          <InfiniteScroll style={{overflow: 'hidden'}} dataLength={this.props.images.length} next={this.props.next} hasMore={true} loader={<div className="loader"><div class="loadingio-spinner-dual-ball-7vhy8l1xg7t"><div class="ldio-9ro5rlpgzop">
-<div></div><div></div><div></div>
-</div></div></div>}>
+          <InfiniteScroll style={{overflow: 'hidden'}} dataLength={this.props.images.length} next={this.props.next} hasMore={true} loader={<div className="loader"><div className="loadingio-spinner-dual-ball-7vhy8l1xg7t"><div className="ldio-9ro5rlpgzop">
+         <div></div><div></div><div></div>
+         </div></div></div>}>
           <div className="image-grid">
-          {this.props.images.map((image, index) => (
+          {this.props.images.map((image) => (
             <div className="image">
-              <a key={index} href={image.links.html} target="_blank" rel="noopener noreferrer" >
-              <img src={image.urls.regular} alt={image.alt_description} />
+              <a href={image.links.html} target="_blank" rel="noopener noreferrer" >
+              <img key={image.id} src={image.urls.regular} alt={image.alt_description} />
             </a>
             </div>
           ))}
-          
           </div>
           </InfiniteScroll>
          </div>
