82a8485befe36bf65ff633e25c822c8b6fea9fcf
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-02-11 23:09:59 +0530

    installed firebase and added firebase config

---
+++ src/firebase/firebase-utils.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCsbe0bAwdTrXtFhn0psb1wKHFSDYVDhcU',
+  authDomain: 'unsplashy-0410.firebaseapp.com',
+  projectId: 'unsplashy-0410',
+  storageBucket: 'unsplashy-0410.appspot.com',
+  messagingSenderId: '301885809347',
+  appId: '1:301885809347:web:8af01b85e0566fefa57876',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
