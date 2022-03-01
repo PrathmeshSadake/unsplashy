166281f69193e40f8d836c26e445ea211ad73d6c
moulibrota-das <dasmoulibrota@gmail.com>
2022-03-01 09:02:29 +0530

    changed the search section text color and shadow

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,25 +1,28 @@
-import * as React from 'react';
-import SearchIcon from '@mui/icons-material/Search';
+import * as React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import style from "./search.module.css";
 
 const InputWithIcon = () => {
-  const [input, setInput] = React.useState('');
+  const [input, setInput] = React.useState("");
   const handleSubmit = () => {
     // alert(input);
   };
   return (
-    <div class='wrap'>
-      <h1>Unsplashy</h1>
-      The internet’s source of freely-usable images. Powered by creators
-      everywhere.
-      <form class='search' onSubmit={handleSubmit}>
+    <div className="wrap">
+      <div className={style.hero_text}>
+        <h1>Unsplashy</h1>
+        The internet’s source of freely-usable images. Powered by creators
+        everywhere.
+      </div>
+      <form class="search" onSubmit={handleSubmit}>
         <input
-          type='text'
-          class='searchTerm'
-          placeholder='What are you looking for?'
+          type="text"
+          className="searchTerm"
+          placeholder="What are you looking for?"
           value={input}
           onChange={(e) => setInput(e.target.value)}
         />
-        <button type='submit' class='searchButton'>
+        <button type="submit" className="searchButton">
           <SearchIcon />
         </button>
       </form>
